609b789f229a1724e4912c723a575782c772b3d4
Jasper Desmet <your.email@example.com>
2026-03-04 14:01:34 +0100

    new files made

--- R/00_run_all.R
+++ R/00_run_all.R
@@ -4,5 +4,11 @@ source("R/01_config.R")
 source("R/02_import_clean.R")
 source("R/03_text_scores.R")
 source("R/04_event_study.R")
+#FF3
+#source("R/04.2_event_study.R")
+#Lagged terms
+#source("R/04.3_event_study.R")
 source("R/05_models_figures.R")
+#source("R/05.2_models_figures.R")
+#source("R/05.3_models_figures.R")
 
